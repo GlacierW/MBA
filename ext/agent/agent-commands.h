b2f769d4170f14acc3ac20f41293f0f04c4061cd
JCJao <jcjao0120@gmail.com>
2016-05-06 12:53:12 +0800

    command w_invk -> w_invo handler do_w_invk -> do_w_invo

--- ext/agent/agent-commands.h
+++ ext/agent/agent-commands.h
@@ -6,7 +6,7 @@ struct QDict;
 void do_win_impo(struct Monitor *mon, const struct QDict *qdict);
 void do_win_expo(struct Monitor *mon, const struct QDict *qdict);
 void do_win_exec(struct Monitor *mon, const struct QDict *qdict);
-void do_win_invk(struct Monitor *mon, const struct QDict *qdict);
+void do_win_invo(struct Monitor *mon, const struct QDict *qdict);
 void do_win_init(struct Monitor *mon, const struct QDict *qdict);
 void do_win_stat(struct Monitor *mon, const struct QDict *qdict);
 #endif
