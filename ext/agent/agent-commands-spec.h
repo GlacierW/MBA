b27016db2b247b972110e96459867ba1a9a6b5c2
JeffreyJao <jcjao0120@gmail.com>
2017-02-12 22:02:04 +0800

    Rename AGENT commands. (PR #35)
    1. Add the 'mba_' prefix to all agent commands
    2. Replace "w_" prefix with "win"

--- ext/agent/agent-commands-spec.h
+++ ext/agent/agent-commands-spec.h
@@ -18,7 +18,7 @@
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 {
-    .name         = "w_impo",
+    .name         = "mba_winimpo",
     .args_type    = "srcpath:F,dstpath:s",
     .params       = "fullpath_host_srcfile fullpath_guest_dstfile",
     .help         = "Import a host file into the guest."
@@ -26,7 +26,7 @@
     .mhandler.cmd = do_win_impo,
 },
 {
-    .name         = "w_expo",
+    .name         = "mba_winexpo",
     .args_type    = "srcpath:s,dstpath:F",
     .params       = "fullpath_guest_srcfile fullpath_host_dstfile",
     .help         = "Export a guest file to the host."
@@ -34,42 +34,42 @@
     .mhandler.cmd = do_win_expo,
 },
 {
-    .name         = "w_exec",
+    .name         = "mba_winexec",
     .args_type    = "cmdline:s",
     .params       = "command_to_execute",
     .help         = "Execute the given command inside the guest and perform interactive Input/Output.",
     .mhandler.cmd = do_win_exec,
 },
 {
-    .name         = "w_invo",
+    .name         = "mba_wininvo",
     .args_type    = "cmdline:s",
     .params       = "command_to_execute",
     .help         = "Invoke the given command inside the guest without waiting the result/output.",
     .mhandler.cmd = do_win_invo,
 },
 {
-    .name         = "w_logf",
+    .name         = "mba_winlogf",
     .args_type    = "dstpath:s",
     .params       = "fullpath_logfile",
     .help         = "Export the agent log file from the guest to the host. The fullpath of the destination file is required.",
     .mhandler.cmd = do_win_logf,
 },
 {
-    .name         = "w_init",
+    .name         = "mba_wininit",
     .args_type    = "",
     .params       = "",
     .help         = "Create an agent thread to communicate with Windows in-VM agent server.",
     .mhandler.cmd = do_win_init,
 },
 {
-    .name         = "w_sync",
+    .name         = "mba_winsync",
     .args_type    = "", 
     .params       = "",
     .help         = "Flush the Guest OS cache.",
     .mhandler.cmd = do_win_sync,
 },
 {
-    .name         = "w_reset",
+    .name         = "mba_winreset",
     .args_type    = "", 
     .params       = "",
     .help         = "Reset the agent client to the condition before w_init.",
